4533a5e8d07930f7f95a40ea04756ca76878cd7f
MOUNIKAMUTTA <mounikamutta1@gmail.com>
2022-12-25 15:58:28 +0530

    added new files

--- src/com/rameshsoft/automation/gitpractice/GitDemo.java
+++ src/com/rameshsoft/automation/gitpractice/GitDemo.java
@@ -1,5 +1,9 @@
 package com.rameshsoft.automation.gitpractice;
 
-public class GitDemo {
-
+public class GitDemo 
+{
+public static void main(String[] args)
+{
+System.out.println("HELLO DO SMART WORK");	
+}
 }
--- src/com/rameshsoft/automation/gitpractice/GitDemoOne.java
+++ src/com/rameshsoft/automation/gitpractice/GitDemoOne.java
@@ -1,5 +1,10 @@
 package com.rameshsoft.automation.gitpractice;
 
-public class GitDemoOne {
-
+public class GitDemoOne 
+{
+public static void main(String[] args)
+{
+System.out.println("SMART WORK + HARD WORK = JOB ");
+System.out.println("job= money");
+}
 }
