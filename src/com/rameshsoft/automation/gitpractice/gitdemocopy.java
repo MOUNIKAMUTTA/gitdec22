53135d0f7648ad30282f6a024b4c5f93a1d6316c
MOUNIKAMUTTA <mounikamutta1@gmail.com>
2022-12-27 08:56:13 +0530

    added new files in gitdemo

---
+++ src/com/rameshsoft/automation/gitpractice/gitdemocopy.java
@@ -0,0 +1,10 @@
+package com.rameshsoft.automation.gitpractice;
+
+public class GitDemo 
+{
+public static void main(String[] args)
+{
+System.out.println("HELLO DO SMART WORK");	
+System.out.println("MOUNIKA");
+}
+}
